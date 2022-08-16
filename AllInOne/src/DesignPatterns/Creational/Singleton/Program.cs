8f6144af8fb5b27b22b57c8014facc1931d2e75e
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-08-16 09:31:08 +0300

    Add Singleton Pattern

--- AllInOne/src/DesignPatterns/Creational/Singleton/Program.cs
+++ AllInOne/src/DesignPatterns/Creational/Singleton/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var dbProcessor = DatabaseProcessor.Create();
+dbProcessor.BackupCount = 2;
+
+var alternateDbProcessor = DatabaseProcessor.Create();
+Console.WriteLine(alternateDbProcessor.BackupCount);
+//Bir nesnenden ugulamanın life-cycle'ı boyunca sadece ve sadece 1 adet olmasını istiyorsak bu patternden faydalanırız!
+
+public class DatabaseProcessor
+{
+    private static DatabaseProcessor processor;
+    private DatabaseProcessor()
+    {
+
+    }
+
+    public int BackupCount { get; set; }
+
+    public static DatabaseProcessor Create()
+    {
+        if (processor == null)
+        {
+            processor = new DatabaseProcessor();
+        }
+
+        return processor;
+    }
+
+}
